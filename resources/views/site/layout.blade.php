997707e833d54e8a1d86fe949c95e39990845952
Vincent Clarysse <claryssevincent@gmail.com>
2023-05-23 10:46:04 +0200

    Created and linked header footer files

---
+++ resources/views/site/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>10 dots</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container">
+
+        @include("site.header")
+
+        <div class="wrapper">
+        @yield ('content')
+        </div>
+
+        @include("site.footer")
+
+    </div>
+</body>
+</html>
