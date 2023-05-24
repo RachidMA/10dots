939138ec87d8534d5dc4a15e04608da65486e9b9
Livara <livara@gmail.com>
2023-05-24 15:22:23 +0200

    Create job search result controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
 
     // public function show(){
     //     return view('welcome');
-    // 
+    // }
+    public function search(Request $request)
+    {
+        $job = $request->input('keyword'); //this keyword is for the form 'name= job'
+        $city = $request->input('city');
+        $country = $request->input('country');
+        
+        $query = Job::query();  
+
+        if($job){
+            $query->where('job_title', 'like', '%' . $job . '%');
+        }
+        if($city){
+            $query->where('city', 'like', '%' . $city . '%');
+        }
+        if($country){
+            $query->where('country', 'like', '%'  . $country. '%');
+        }
+
+        $searchResult = $query->get();
+
+        return view ('job_searchresults', $searchResult);
+    }
+    
 }
