1489dea38371b9178f7cd6b75cd60e482688d064
Ada Omoji <ada.omoji@gmail.com>
2023-05-26 14:59:05 +0200

    Empty to switch to another branch

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,22 +17,5 @@ use Illuminate\Http\Request;
 //     }
 // }
 
-// class UserController extends Controller
-// {
-//     public function show () {
-//          $jobs = Job::all();
-//          return view ("userDetails", ['jobs'=>$jobs]);
-//     }
-   
-// }
 
 
-
-class UserController extends Controller
-{
-    public function show($id) {
-        $user = Job::findOrFail($id); // Retrieve the user by ID
-        
-        return view("site.userDetails", ['user' => $user]);
-    }
-}
