918b7de842b8d1397dd81504c51f3dae7b8ccd47
rachid <azzahir421@gmail.com>
2023-05-26 14:17:13 +0100

    Install laravel ui and auth

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomepageController;
 
 use App\Http\Controllers\UserController;
-
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,13 +23,20 @@ use App\Http\Controllers\UserController;
 //Landing Page Routes
 Route::get('/', [HomepageController::class, 'show'])->name("homepage");
 
-Route::view('/form','Testing.Job_search_form');
+Route::view('/form', 'Testing.Job_search_form');
 
 //Routes for jobs
-Route::get('/jobs', [JobController::class,'showAllJobs'])->name('showAll-jobs');
+Route::get('/jobs', [JobController::class, 'showAllJobs'])->name('showAll-jobs');
 Route::post('/search-job', [JobController::class, 'search'])->name('search-result');
 
 
 
 //Routes for users
 Route::get('/user/{id}', [UserController::class, 'show'])->name('showUserDetails');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
