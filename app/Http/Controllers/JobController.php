cf11db83376d51708b07d468f0a8459641d6d8b4
rachid <azzahir421@gmail.com>
2023-05-30 14:14:23 +0100

    Add route, controller and Category model to store job in database

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Job;
 
@@ -34,6 +35,7 @@ class JobController extends Controller
             ->where('country', $country);
         $searchResult = $query->get();
 
+        // dd($searchResult);
 
         return view('testing.Jobstesting', ['searchResult' => $searchResult]);
         // if($searchResult->isEmpty()){
@@ -44,4 +46,95 @@ class JobController extends Controller
         // }
     }
 
+    //RACHID:CREATE JOB BY AUTHENTICATED USER
+    public function createJob(Request $request)
+    {
+        //fetch all existing job categories
+        $categories = Category::all();
+
+        // TODO: FETCH COUNTRIES AND PASS THEM WITH VIEW FOR COUNTRIES DROPDOWN FIELD
+
+
+        //return create job form
+        return view('testing.job_create_form')->with('categories', $categories);
+    }
+    //RACHID:STORE JOB BY AUTHENTICATED USER
+    public function storeJob(Request $request)
+    {
+        // dd($request);
+        //Validate form data
+        $request->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'phone' => ['required', 'regex:/[0-9+\-\(\)\s]*$/'],
+            'address' => ['required'],
+            'country' => ['required'],
+            'city' => ['required'],
+            'category' => ['required'],
+            'job_title' => ['required'],
+            'description' => ['required'],
+            'image_url' => ['image', 'mimes:jpeg, png, jpg, gif|max:2048'],
+        ]);
+
+
+        //GET DATA FROM REQUEST AFTER IT PASSES VALIDATION 
+        $job_data = $request->all();
+
+        // ///Get the category_id based on category name(We use this method because we need to include category name in error message)
+        // $category_id = Category::where('name', $job_data['category']);
+
+
+        //Check if we have uploaded image
+        if ($image = $request->file('image_url')) {
+            //Get File Name With Extension
+            $image_name = time() . '-' . $image->getClientOriginalName();
+            $image->move(public_path('images'), $image_name);
+
+            //Update the image name(path) inside $request['image_url']
+            $job_data['image_url'] = $image_name;
+        } else {
+            //Store a default image in public/images
+            $job_data['image_url'] = 'default.jpg';
+        }
+
+        // //CHECK IF THERE IS MIN_PRICE AND MAX_PRICE
+        // if ($request->min_price && $request->max_price) {
+        //     //IF MIN_PRICE AND MAX_PRICE EXIST
+        //     //CHECK IF MIN_PRICE > MAX_PRICE
+        //     if ($request->min_price > $request->max_price) {
+        //         //IF MIN_PRICE > MAX_PRICE
+        //         //RETURN ERROR MESSAGE
+        //         return redirect()->back()->with('error', 'Min price should be less than max price');
+        //     } else {
+        //         //IF MIN_PRICE < MAX_PRICE
+        //         //UPDATE MIN_PRICE AND MAX_PRICE
+        //         $job_data['min_price'] = $request->min_price;
+        //         $job_data['max_price'] = $request->max_price;
+        //     }
+        // } else {
+        //     $job_data['min_price'] = null;
+        //     $job_data['max_price'] = null;
+        // }
+
+        //STORE THE DATA INTOR DATABASE
+        Job::insert([
+            'first_name' => $job_data['first_name'],
+            'last_name' => $job_data['last_name'],
+            'phone' => $job_data['phone'],
+            'address' => $job_data['address'],
+            'country' => $job_data['country'],
+            'city' => $job_data['city'],
+            'job_title' => $job_data['job_title'],
+            'description' => $job_data['description'],
+            'min_price' => $job_data['min_price'],
+            'max_price' => $job_data['max_price'],
+            'image_url' => $job_data['image_url'],
+            'user_id' => auth()->id(),
+            'category_id' => $job_data['category'],
+        ]);
+
+        dd('ALL INPUTS FROM FORM', $job_data);
+
+        // return redirect('site.userDashoard')->with('success', 'Job created successfully');
+    }
 }
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,4 +2,7 @@
     <a href="#">Home</a>
     <a href="#">Job Details</a>
     <a href="#">link</a>
+
+    <!-- RACHID: I ADDED THIS LINK TO WORK ON CRAETE JOB FORM(WILL BE MOVE LATE TO THE DOES PAGE) -->
+    <a href="{{route('create-job')}}">Create Job</a>
 </div>
