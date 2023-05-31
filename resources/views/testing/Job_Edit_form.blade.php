71c7581a3bf4a8f10979d36a076130a1b07047c5
Ada Omoji <ada.omoji@gmail.com>
2023-05-31 15:59:54 +0200

    Create route and view to delete user profile

--- resources/views/testing/Job_Edit_form.blade.php
+++ resources/views/testing/Job_Edit_form.blade.php
@@ -1,6 +1,6 @@
 <h1>Doer Information</h1>
 
-<table>
+<table border = "1">
     <tr>
         <td>Id</td>
         <td>First name</td>
@@ -14,4 +14,20 @@
         <td>phone</td>
         <td>description</td>
     </tr>
+        @foreach ($doers as $doer)
+        <tr>
+            <td>{{ $doer['id'] }}</td>
+            <td>{{ $doer[ 'first_name' ] }}</td>
+            <td>{{ $doer[ 'last_name' ] }}</td>
+            <td>{{ $doer[ 'address' ] }}</td>
+            <td>{{ $doer[ 'country' ] }}</td>
+            <td>{{ $doer[ 'city' ] }}</td>
+            <td>{{ $doer[ 'job_title' ] }}</td>
+            <td>{{ $doer[ 'price_range' ] }}</td>
+            <td>{{ $doer[ 'image_url' ] }}</td>
+            <td>{{ $doer[ 'phone' ] }}</td>
+            <td>{{ $doer[ 'description' ] }}</td>
+            <td><a href = "{{  route ('deleteJob', [ 'id'=>$doer->id]) }}">Delete</a></td>
+        </tr>
+        @endforeach
 </table>
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,14 @@ Route::get('/jobs', [JobController::class, 'jobs'])->name('jobs');
 Route::get('/jobs/{id}', [JobController::class, 'jobDetails'])->name('jobDetails');
 Route::get('/update-form', [JobController::class, 'editJob'])->name('jobEdit');
 Route::get('/update-form/{id}', [JobController::class, 'updateJob'])->name('jobUpdate');
-Route::view('list', 'testing.Job_edit_form');
+// Route::view('list', 'testing.Job_edit_form');
+
+//Routes to edit doer profiles
+Route::get('/list', [JobController::class, 'list'])->name('editJob');
+Route::get('/delete/{id}', [JobController::class, 'delete'])->name('deleteJob');
+
+// Route::get('/list', [JobController::class, 'editJob'])->name('editJob');
+// Route::get('/delete/{id}', [JobController::class, 'delete'])->name('deleteJob');
 
 //THESE ROUTE SHOULD BE ALLOWED ONLY FOR DOERS(===RACHID ADDED THIS ROUTES===)
 Route::prefix('/user')->middleware(['auth'])->group(function () {
