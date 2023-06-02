0043961fc2075bd00ca2a294e15b5d21b9eb2306
Ada Omoji <ada.omoji@gmail.com>
2023-06-02 23:22:40 +0200

    Create update job route, view, and controller

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -24,7 +24,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route ('jobEdit') }}">
+                    <a class="nav-link" href="#">
                         Update Job
                     </a>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -41,20 +41,16 @@ Route::post('/contact-us', [ContactController::class, 'store'])->name('contact.s
 //==============Routes to get jobs (ADA)====================//
 Route::get('/jobs', [JobController::class, 'jobs'])->name('jobs');
 Route::get('/jobs/{id}', [JobController::class, 'jobDetails'])->name('jobDetails');
-Route::get('/update-form', [JobController::class, 'editJob'])->name('jobEdit');
-Route::get('/update-form/{id}', [JobController::class, 'updateJob'])->name('jobUpdate');
-// Route::view('list', 'testing.Job_edit_form');
 
 
 //=============Routes to edit doer profiles (ADA)==========//
 Route::get('/list', [JobController::class, 'list']);
 Route::get('/delete/{id}', [JobController::class, 'delete'])->name('deleteJob');
 Route::get('/edit/{id}', [JobController::class, 'editJob'])->name('editJob');
-Route::get('/update/{id}', [JobController::class, 'updateJob'])->name('updateJob');
-// Route::post('/update', [JobController::class, 'updateJob'])->name('updateJob');
+Route::get('/update/{id}', [JobController::class, 'showJob'])->name('showJob');
+Route::post('/update/{id}', [JobController::class, 'updateJob'])->name('updateJob');
+Route::get('/testing.Job_success', [JobController::class, 'redirect'])->name('redirect');
 
-// Route::get('/list', [JobController::class, 'editJob'])->name('editJob');
-// Route::get('/delete/{id}', [JobController::class, 'delete'])->name('deleteJob');
 
 //THESE ROUTE SHOULD BE ALLOWED ONLY FOR DOERS(===RACHID ADDED THIS ROUTES===)
 Route::prefix('/user')->middleware(['auth'])->group(function () {
@@ -65,7 +61,7 @@ Route::prefix('/user')->middleware(['auth'])->group(function () {
     Route::get('/{id}/create-job', [JobController::class, 'createJob'])->name('create-job');
     Route::post('/store-job', [JobController::class, 'storeJob'])->name('store-job');
 });
-
+ 
 
 //AUTHENTICATION ROUTES
 Auth::routes();
