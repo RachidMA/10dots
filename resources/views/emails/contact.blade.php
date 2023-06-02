0bca5647c0efef3f78cff61ed6c62b390605e2f7
Livara <livara@gmail.com>
2023-06-02 20:59:07 +0200

    Create a template of the emails.contact

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,3 +1,23 @@
-"The purpose of our lives is to be happy."
-
-<!-- Design this please -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Email</title>
+</head>
+<body>
+    <h1>Contact Email</h1>
+    <p>Hello,</p>
+    <p>Thank you for contacting us. We have received your message and will get back to you soon.</p>
+    
+    <h2>Message Details:</h2>
+    <ul>
+        <li><strong>Name:</strong> {{ $contact->name }}</li>
+        <li><strong>Email:</strong> {{ $contact->email }}</li>
+        <li><strong>Message:</strong> {{ $contact->message }}</li>
+    </ul>
+    
+    <p>Best regards,</p>
+    <p>Your Company</p>
+</body>
+</html>
