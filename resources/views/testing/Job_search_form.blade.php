345ebf359f85b97eb7b6a56263261fdea84e111f
Livara <livara@gmail.com>
2023-06-04 12:36:56 +0200

    Create the show function to show the job search form

--- resources/views/testing/Job_search_form.blade.php
+++ resources/views/testing/Job_search_form.blade.php
@@ -7,14 +7,12 @@
     <label for="job">Job:</label>
     <input type="text" id="job" name="job"><br><br>
 
-    <label for="city">City:</label>
-    <input type="text" id="city" name="city"><br><br>
 
     <label for="country">Country:</label>
     <input type="text" id="country" name="country"><br><br>
 
-    <label for="price_min">Minimum Price:</label>
-    <input type="text" id="price_min" name="price_min"><br><br>
+    <!-- <label for="price_min">Minimum Price:</label>
+    <input type="text" id="price_min" name="price_min"><br><br> -->
     
     <label for="price_max">City:</label>
     <input type="text" id="price_max" name="price_max"><br><br>
