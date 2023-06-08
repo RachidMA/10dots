55cbd11979f97414d3560d03daa4fefa2c4b9e6c
rachid <azzahir421@gmail.com>
2023-06-08 21:25:04 +0100

    Style job details view and add spam button

--- resources/views/testing/Job_detail.blade.php
+++ resources/views/testing/Job_detail.blade.php
@@ -2,70 +2,107 @@
 
 @section('content')
 
-<h1>Job Details</h1>
-<p>Job Title: {{ $job->job_title }}</p>
-<p>Name: {{ $job->first_name }} {{ $job->last_name }}</p>
-<p>Address: {{ $job->address }}</p>
-<p>City: {{ $job->city }}</p>
-<p>Country: {{ $job->country }}</p>
-<p>Image: {{ $job->image_url}}</p>
-<p>Description: {{$job->description}}</p>
-<p>Price: {{ $job->min_price }} - {{ $job->max_price}}</p>
-
-
-<button><a href="{{ route('editJob', ['id' => $job->id]) }}" class="btn btn-primary">Edit</a></button>
-<button><a href="{{ route('deleteJob', ['id' => $job->id]) }}" class="btn btn-primary">Delete</a></button>
-
-<div class="buttons-container">
-    <button>Book now</button>
-    <button>Contact me</button>
+<div class="job-main-content">
+    <div class="doer-job-details">
+        <div class="doer-job-details-card">
+            <div class="job-image job-image-guest">
+                <img class="card-img-top" src=" /images/{{$job->image_url}}" alt="Card image cap">
+            </div>
+            <div class="job-informations">
+                <div class="job-title">
+                    <h3 class="title">Job Title: {{ $job->job_title }}</h3>
+                </div>
+                <div class="job-info-text">
+                    <p>{{ $job->first_name }} {{ $job->last_name }}</p>
+                    <p>Address: {{ $job->address }}</p>
+                    <div class="job-country-city">
+                        <p class="country"> {{ $job->country }}</p>
+                        <p>{{ $job->city }}</p>
+                    </div>
+                    <p>Price: {{ $job->min_price }} - {{ $job->max_price}}</p>
+                    <h3>{{$job->user->id}}</h3>
+                </div>
+                <div class="contact-me">
+                    <a href="#">Contact Me</a>
+                    <button class="reviewButton">Leave a Review</button>
+                </div>
+            </div>
+        </div>
+        <div class="job-description">
+            <p>Description: {{$job->description}}</p>
+        </div>
+    </div>
+    <div class="spam-button">
+        <a id="reportButton">Report Profile</a>
+    </div>
 </div>
 
 <div id="reviewFormContainer" class='review'>
-
-    <button>Leave a Review</button>
-
     <form method="POST" action="{{ route ('leaveReview') }}">
         @csrf
 
         <h1>Leave a review</h1>
+        <div class="main-review-form">
+            <div class="mb-3 name-email">
+                <!-- <label for="name" class="form-label">Your Name</label> -->
+                <input type="text" name="name" placeholder="enter your name here" id="name">
 
-        <div class="mb-3">
-            <label for="name" class="form-label">Your Name</label>
-            <input type="text" name="name" placeholder="enter your name here" id="name">
+                <!-- <label for="name" class="form-label">Your Email</label> -->
+                <input type="email" name="email" placeholder="enter your email here" id="email">
+            </div>
+            <div class="mb-3">
+                <label for="comment" class="form-label">Comments</label>
+                <textarea id="textarea" rows="3" cols="25" name="comment" id="comment"></textarea>
+            </div>
+            <label for="rate" class="form-label">Rate</label>
+            <div class="rate">
 
-            <label for="name" class="form-label">Your Email</label>
-            <input type="email" name="email" placeholder="enter your email here" id="email">
+                <input type="radio" id="star5" name="rating" value="5" />
+                <label for="star5" title="text">5 dots</label>
+
+                <input type="radio" id="star4" name="rating" value="4" />
+                <label for="star4" title="text">4 dots</label>
+
+                <input type="radio" id="star3" name="rating" value="3" />
+                <label for="star3" title="text">3 dots</label>
+
+                <input type="radio" id="star2" name="rating" value="2" />
+                <label for="star2" title="text">2 dots</label>
+
+                <input type="radio" id="star1" name="rating" value="1" />
+                <label for="star1" title="text">1 dot</label>
+            </div>
+            <input type="hidden" name="job_id" id="job_id" value="6">
+
+            <div class="submit-button">
+                <button type="submit" name="submit" id="submit">Submit</button>
+            </div>
         </div>
 
-        <div class="rate">
-            <input type="radio" id="star5" name="rating" value="5" />
-            <label for="star5" title="text">5 dots</label>
 
-            <input type="radio" id="star4" name="rating" value="4" />
-            <label for="star4" title="text">4 dots</label>
-
-            <input type="radio" id="star3" name="rating" value="3" />
-            <label for="star3" title="text">3 dots</label>
-
-            <input type="radio" id="star2" name="rating" value="2" />
-            <label for="star2" title="text">2 dots</label>
-
-            <input type="radio" id="star1" name="rating" value="1" />
-            <label for="star1" title="text">1 dot</label>
-        </div>
-
-        <div class="mb-3">
-            <label for="comment" class="form-label">Comments</label>
-            <textarea id="textarea" rows="3" cols="25" name="comment" id="comment"></textarea>
-        </div>
-
-        <input type="hidden" name="job_id" id="job_id" value="6">
-
-        <button type="submit" name="submit" id="submit">Submit</button>
 
     </form>
 </div>
+<div id="reportModal" class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Confirmation</h5>
+            <span id="closeModal" class="close">&times;</span>
+        </div>
+        <div class="modal-body">
+            <p>By clicking OK, you are reporting this profile as a spammer.</p>
+        </div>
+        <div class="modal-footer">
+            <button id="cancelButton" class="btn">No</button>
+            <form method="POST" action="">
+                @csrf
+                <input type="hidden" name="job_id" value="{{ $job->id }}">
+                <button type="submit" class="btn btn-danger" id="confirmButton">OK</button>
+            </form>
 
+
+        </div>
+    </div>
+</div>
 
 @endsection
