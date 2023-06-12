68953099ca472a9cae399a77a56b2e6053a3d228
Vincent Clarysse <claryssevincent@gmail.com>
2023-06-12 16:32:44 +0200

    doer_dash_styling

--- resources/views/testing/Doer_jobdetails.blade.php
+++ resources/views/testing/Doer_jobdetails.blade.php
@@ -1,58 +1,41 @@
 @extends ('layout.layout')
 
 @section('content')
-<div class="job-main-content">
-    <div class="doer-job-details">
-        <div class="doer-job-details-card">
-            <div class="job-image">
-                <img class="card-img-top" src="/images/{{$job->image_url ? $job->image_url:'job_default.png'}}" alt="Card image cap">
-            </div>
-            <!-- REUSE THIS FORM TO UPLOAD JOB IMAGE 
-    IS BETTER TO HAVE USER UPLOAD IMAGE AFTER JOB IS CREATED NOT WHEN HE IS CREATING THE JOB
-    JUST MINIMIZE ISSUES -->
-            <div class="upload">
-                <form action="{{route('upload-job-image', ['id'=>$job->id])}}" method="POST" enctype="multipart/form-data" id="image-upload">
-                    @csrf
-                    <div class="round">
-                        <i class="fa fa-camera"></i>
-                        <input id="file-upload" type="file" name='avatar' />
+<div class="job_details">
+        <div class="img_div">
+            <img class="card-img-top" src="/images/{{$job->image_url ? $job->image_url:'job_default.png'}}" alt="Card image cap">
+        </div>
+        <div class="upload">
+            <form action="{{route('upload-job-image', ['id'=>$job->id])}}" method="POST" enctype="multipart/form-data" id="image-upload">
+                @csrf
+                <div class="round">
+                    <i class="fa fa-camera"></i>
+                    <input id="file-upload" type="file" name='avatar' />
 
-                        <button type="submit" class="btn btn-primary" id="submit">Upload</button>
-                        @error('avatar')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </form>
-            </div>
-            <div class="job-informations">
-                <div class="job-title">
-                    <h3 class="title">Job Title: {{ $job->job_title }}</h3>
+                    <button type="submit" class="btn btn-primary" id="submit">Upload</button>
+                    @error('avatar')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
-                <div class="job-info-text">
-                    <p>{{ $job->first_name }} {{ $job->last_name }}</p>
-                    <p>Address: {{ $job->address }}</p>
-                    <div class="job-country-city">
-                        <p class="country"> {{ $job->country }}</p>
-                        <p>{{ $job->city }}</p>
-                    </div>
-                    @if($job->price)
-                    <p class="price">Price: {{$job->price}} €</p>
-                    @else
-                    <p>Price: No Price Was Set</p>
-                    @endif
-                </div>
-            </div>
+            </form>
         </div>
-        <div class="job-description">
-            <p>Description: {{$job->description}}</p>
-            <div class="edit-delete">
-                <a href="{{ route('editJob', ['id' => $job->id]) }}">Edit</a>
-                <a href="{{ route('deleteJob', ['id' => $job->id]) }}" class="delete">Delete</a>
-            </div>
+        <div class="job-informations">
+            <h3 class="title">Job Title: {{ $job->job_title }}</h3>
+            <p>{{ $job->first_name }} {{ $job->last_name }}</p>
+            <p>Address: {{ $job->address }}</p>
+            <p class="country"> {{ $job->country }}</p>
+            <p>{{ $job->city }}</p>
+            @if($job->price)
+            <p class="price">Price: {{$job->price}} €</p>
+            @else
+            <p>Price: No Price Was Set</p>
+            @endif
         </div>
-    </div>
+        <p>Description: {{$job->description}}</p>
+        <a href="{{ route('editJob', ['id' => $job->id]) }}">Edit</a>
+        <a href="{{ route('deleteJob', ['id' => $job->id]) }}" class="delete">Delete</a>
 </div>
 
 @endsection
