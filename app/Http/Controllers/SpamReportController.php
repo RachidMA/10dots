2eb2c8989c01554b9f5f5a24368a88e2f6fde8b6
Livara <livara@gmail.com>
2023-06-13 12:33:36 +0200

    Delete comments in SpamController

--- app/Http/Controllers/SpamReportController.php
+++ app/Http/Controllers/SpamReportController.php
@@ -16,30 +16,17 @@ use Illuminate\Support\Facades\Notification;
 
 class SpamReportController extends Controller
 {
-    //
-    //RACHID:ADD REPORT SPAM CONTROLLER
     public function reportSpam(Request $request)
     {
         $jobId = $request->input('job_id');
-
-        // Fetch the job
         $job = Job::find($jobId);
         $doer_id = $job->user_id;
 
-
-
-        // Check if the job exists
         if ($job) {
-            // Fetch the user associated with the job
             $user = User::find($doer_id);
 
-            // Check if the user exists
             if ($user) {
-                // Increment the spam report count
                 $user->increment('spam_reports');
-
-
-                // Get the current spam report count
                 $spamReportCount = $user->spam_reports;
                 $data = [
                     "admin_email" => "10dots@gmail.com",
@@ -48,33 +35,25 @@ class SpamReportController extends Controller
                     "spamReportCount" => $spamReportCount
                 ];
 
-                // Check the spam report count
                 if ($spamReportCount == 2) {
-                    // Send email to admin about spam report count being 2
                     Mail::to($user->email)->send(new SpamReportEmail($data));
 
-                    //Send notification to admin
                     Notification::route('mail', $data['admin_email'])->notify(new SpamReportNotification($data));
                 } elseif ($spamReportCount >= 5) {
-                    // Send email to admin about spam report count being 5 or more
+
                     Mail::to($user->email)->send(new SendDoerDeleteEmail($data));
 
-                    //Send notification to admin that the doer account has been closed
                     Notification::route('mail', $data['admin_email'])->notify(new ProfileDeleteNotification($data));
 
-                    // Delete the user
                     $user->delete();
 
-                    // Redirect or return a response
                     return redirect()->route('homepage');
                 }
 
-                // Redirect or return a response
                 return redirect()->back()->with('success', 'User reported as spam successfully.');
             }
         }
 
-        // Redirect or return a response if the job or user was not found
         return redirect()->back()->with('error', 'Failed to report user as spam.');
     }
 }
