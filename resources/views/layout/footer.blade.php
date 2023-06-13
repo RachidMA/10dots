ebd6191b16b92bcbc435743e9544604d69cbeb9b
Ada Omoji <ada.omoji@gmail.com>
2023-06-13 09:46:00 +0200

    Add 10dots privacy policy to footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,4 +2,6 @@
     <p>© 10dots</p>
     
     <a class="nav-link" href="{{route('contact.create')}}">Contact Us</a>
+    <a class="nav-link" href="https://www.privacypolicies.com/live/2fe111c5-29e4-4310-8f3e-0de59ac47308">Privacy Policy</a>
+
 </div>
