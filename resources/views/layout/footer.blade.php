d9c6eabe5b909de1b612ab35361b57e762eb2ac0
Livara <livara@gmail.com>
2023-06-14 10:43:24 +0200

    Add style to the footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,10 @@
 <div class="footer">
-    <p>© 10dots</p>
 
+    <a class="nav-link dash" href="{{route('about-us')}}">About Us</a>
     <a class="nav-link" href="{{route('contact.create')}}">Contact Us</a>
     <a class="nav-link" href="https://www.privacypolicies.com/live/2fe111c5-29e4-4310-8f3e-0de59ac47308">Privacy Policy</a>
 
+<div class="copyright">
+    © 2023 10dots 
+</div>
 </div>
