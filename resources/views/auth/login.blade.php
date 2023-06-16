47a131b053dc1f8fdf98d13536a3ebd07299a2cd
rachid <azzahir421@gmail.com>
2023-06-16 09:50:11 +0100

    Modify job default page and change upload image position

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
 <div class="loreg_card">
-<div class="big_circ_2"></div>
+    <div class="big_circ_2"></div>
 
     <h2>login</h2>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder ="email"required autocomplete="email" autofocus>
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="email" autocomplete="email" required autofocus>
 
         @error('email')
         <span class="invalid-feedback" role="alert">
@@ -18,7 +18,7 @@
         </span>
         @enderror
 
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder ="password" required autocomplete="current-password">
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autocomplete="current-password">
 
         @error('password')
         <span class="invalid-feedback" role="alert">
