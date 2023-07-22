104d59a4d2f48a87ef9355076d437916a21d0477
rachid <azzahir421@gmail.com>
2023-07-22 20:40:51 +0100

    Remove duplicate booking id from migrations

--- database/migrations/2023_07_17_171904_create_bookings_table.php
+++ database/migrations/2023_07_17_171904_create_bookings_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->increments('id');
-            $table->increments('id');
             $table->string('customer_name');
             $table->string('customer_email');
             $table->unsignedInteger('job_id');
