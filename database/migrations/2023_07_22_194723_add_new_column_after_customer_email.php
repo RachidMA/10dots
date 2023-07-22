3720cbf38017e51ae30742b51163b2ac5354e407
rachid <azzahir421@gmail.com>
2023-07-22 20:51:04 +0100

    Add customer number to bookings table

---
+++ database/migrations/2023_07_22_194723_add_new_column_after_customer_email.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            // Replace 'new_column_name' with the name of the new column you want to add.
+            $table->string('customer_number')->after('customer_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
+    }
+};
